6adb0f865f4665f8fe844618e717ceb8d7c10804
sajwan05 <surisajwan1998@gmail.com>
2025-09-07 17:11:06 +0000

    Solved 9th excercise of odin

--- javascript.js
+++ javascript.js
@@ -4956,6 +4956,49 @@ let listOfNumbers = [2, 3, 5, 7, 11];
 console.log(listOfNumbers[2]);
 console.log(listOfNumbers[0]);
 
+// sumall
+
+// const sumAll = function(start, end) {
+//     let sum = 0;
+//     if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+//         return "ERROR";
+//     } 
+//     else if (start > end) {
+//         for (let j = end; j <= start; j++) {
+//             sum += j;
+//         }
+//     }else{
+//          for(let i = start; i <= end; i++) {
+//         sum += i;
+//     }
+//     }
+   
+
+//     return sum;
+// };
+
+
+// Alternative Approach:
+
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return `ERROR`;   
+    if (min < 0 || max < 0) return `ERROR`;
+
+    if (min > max) {
+        let t = min;
+        min = max;
+        max = t;
+    }
+
+    let sum = 0;
+
+    for (let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum;
+}
+
 
 
 
