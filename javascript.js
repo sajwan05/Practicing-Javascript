54f688b9a422b40480862d8eeeec9899805db9d6
sajwan05 <surisajwan1998@gmail.com>
2025-09-10 13:40:39 +0000

    String methods practice

--- javascript.js
+++ javascript.js
@@ -5449,6 +5449,31 @@ console.log(removeAtIndexCopy(a, a.length - 1));
 
 
 
+// Strings and their properties:
+
+// let Kim = "kim";
+// Kim.age = 99;
+// console.log(Kim.age);
+
+console.log("coconuts".slice(4, "coconuts".length - 1));
+
+console.log("coconuts".indexOf("u"));
+
+console.log("one two three".indexOf("ee"));
+
+console.log("  okay \n".trim());
+
+console.log(String(6).padStart(3, "0"));
+
+let sentence = "Secretarybirds specialize in stomping";
+let words = sentence.split(" ");
+console.log(words);
+
+console.log(words.join(". "));
+
+console.log("LA".repeat(3));
+
+
 
 
 
