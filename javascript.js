a08de6eb4e11d52cfb558fab26d580220747d1e5
sajwan05 <surisajwan1998@gmail.com>
2025-09-10 05:57:34 +0000

    Eloquent js objects and array practice

--- javascript.js
+++ javascript.js
@@ -5218,6 +5218,60 @@ const stopWatch = {
     }
 }
 
+// Eloquent JS chapter 4
+// Data sets 
+// 
+listOfNumbers = [2, 3, 5, 7, 11];
+
+console.log(listOfNumbers[0]);
+console.log(listOfNumbers[listOfNumbers.length - 1]);
+console.log(listOfNumbers["1"]);
+
+// Methods 
+let doh = "Doh";
+console.log(doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+
+console.log(sequence);
+console.log(sequence.pop());
+console.log(sequence);
+
+// Objects:
+
+let day1 = {
+    squirrel: false,
+    events: ["work", "touched tree", "pizza", "running"],
+    "2days": false,
+};
+
+console.log(day1.squirrel);
+console.log(day1.wolf);
+
+day1.wolf = false;
+console.log(day1.wolf);
+
+console.log(day1["2days"]);
+
+let descriptions = {
+    work: "Went to work",
+    "touched tree": "Touched a tree",
+};
+
+
+let anObject = {left: 1, right: 2};
+
+console.log(anObject.left);
+
+delete anObject.left;
+console.log(anObject.left);
+console.log("left" in anObject);
+console.log("right" in anObject);
+
 
 
 
