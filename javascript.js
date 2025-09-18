035f158faa22048cec3941a5800e377dc3ce2e70
sajwan05 <surisajwan1998@gmail.com>
2025-09-18 08:44:04 +0000

    Factory method to create object

--- javascript.js
+++ javascript.js
@@ -6261,3 +6261,21 @@ console.log(circle.radius);
 
 circle.draw();
 
+// Factory method to create objects 
+
+
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {
+            console.log('draw');
+        }
+    }
+}
+
+const circle1 = createCircle(1);
+
+console.log(circle1.radius);
+circle1.draw()
+
